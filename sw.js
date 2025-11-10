c84c594fe6368a508b1794db96f9bd41a7c7e972
Paradispartiet <paradispartiet@gmail.com>
2025-11-11 00:45:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // === HISTORY GO – SW.JS (v1.0, offline + cache-first) =======
 // ============================================================
 
-const VERSION = "v1.0.06";
+const VERSION = "v1.0.07";
 const STATIC_CACHE = `hg-static-${VERSION}`;
 const RUNTIME_CACHE = `hg-runtime-${VERSION}`;
 
