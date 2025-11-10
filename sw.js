4a0727ee9643e2c367786f182850a2cd76d04052
Paradispartiet <paradispartiet@gmail.com>
2025-11-10 02:54:28 +0100

    Update service worker version to hg-v18.0.90

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // For GitHub Pages og lokal testing
 // =====================================================
 
-const V = 'hg-v18.0.89';
+const V = 'hg-v18.0.90';
 const CORE = [
   'index.html',
   'profile.html',
