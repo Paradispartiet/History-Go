bd3e181c4fd4655c176424a0033029871e700cfa
Paradispartiet <paradispartiet@gmail.com>
2025-11-18 10:19:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // For GitHub Pages og lokal testing
 // =====================================================
 
-const V = 'hg-v18.2.1.1.290';
+const V = 'hg-v18.2.1.1.291';
 const CORE = [r
   'index.html',
   'profile.html',
