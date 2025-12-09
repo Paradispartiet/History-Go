e9b5af14aa7fbb527c5e1082efbf5d731622af9f
Paradispartiet <paradispartiet@gmail.com>
2025-12-09 23:37:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Versjon: HG-FULL-v3.0.3.1.002
 ============================================================ */
 
-const CACHE_VERSION = "HG-FULL-v3.0.3.1.011";
+const CACHE_VERSION = "HG-FULL-v3.0.3.1.012";
 const STATIC_CACHE = `historygo-${CACHE_VERSION}`;
 
 /* ------------------------------------------------------------
