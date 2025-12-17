9c1f6bc9d63b052a89d3adf28e0633ce7b43fdb9
Paradispartiet <paradispartiet@gmail.com>
2025-12-17 13:09:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Version: HG-FULL-v3.0.5
 ============================================================ */
 
-const CACHE_VERSION = "HG-FULL-v3.0.025";
+const CACHE_VERSION = "HG-FULL-v3.0.026";
 const STATIC_CACHE = `historygo-${CACHE_VERSION}`;
 
 // Viktig: bruk ABSOLUTTE paths når du har mapper (/css, /js, /data)
