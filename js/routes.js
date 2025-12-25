dd064e4537a7b214e2a3ca57a380c839819a4078
Paradispartiet <paradispartiet@gmail.com>
2025-12-25 22:17:44 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -20,7 +20,7 @@ console.log("routes.js start");
 // -------------------- Config --------------------
 const HG_ORS = {
   baseUrl: "https://api.openrouteservice.org",
-  apiKey: "PUTT_ORS_KEY_HER",     // <-- SETT INN DIN NØKKEL
+  apiKey: apiKey: "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijg1NjAxMzZmNDg0ZDQ0NzM4OTFlMWU1ODJjMjE5NzZlIiwiaCI6Im11cm11cjY0In0=",
   profile: "foot-walking"
 };
 
