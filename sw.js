4290f258ed4cacc270178364e70501af9c772490
Paradispartiet <paradispartiet@gmail.com>
2025-12-26 10:33:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Version: HG-FULL-v3.0.057
 ============================================================ */
 
-const CACHE_VERSION = "HG-FULL-v3.0.183";
+const CACHE_VERSION = "HG-FULL-v3.0.184";
 const STATIC_CACHE = `historygo-${CACHE_VERSION}`;
 
 // Scope path (viktig på GitHub Pages: /History-Go/)
