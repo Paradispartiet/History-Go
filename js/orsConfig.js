63211a0ec826fcda8836b930c1e0c9653504a093
Paradispartiet <paradispartiet@gmail.com>
2025-12-26 08:52:36 +0100

    Create orsConfig.js

---
+++ js/orsConfig.js
@@ -0,0 +1,5 @@
+window.HG_ORS = {
+  baseUrl: "https://api.openrouteservice.org",
+  apiKey: "eyJvcmciOiJIYjNjZTMl0Tc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijg1NjAxMzZmNDg0ZDQ0NzM4OTFlMWU1ODJjMjE5NzIi...",
+  profile: "foot-walking"
+};
