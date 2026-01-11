859b937357bd2cb53d2f36b253e449beea484943
Paradispartiet <paradispartiet@gmail.com>
2026-01-11 02:38:22 +0100

    Update popup-utils.js

--- js/popup-utils.js
+++ js/popup-utils.js
@@ -839,6 +839,7 @@ const peopleHere = (typeof getPeopleForPlace === "function")
 
       <button class="hg-quiz-btn" data-quiz="${place.id}">Ta quiz</button>
 
+      ${wkDocHtml}
       ${wkHtml}
       ${chambersHtml}
     
