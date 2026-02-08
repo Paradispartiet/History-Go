d79e1b92adf207a8dc3b170171747bbdda022be9
Paradispartiet <paradispartiet@gmail.com>
2026-02-08 11:13:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2045,12 +2045,24 @@ function safeRun(label, fn) {
     if (out && typeof out.then === "function") {
       out.catch((e) => {
         console.error(`[${label}]`, e);
-        if (DEBUG) window.__HG_LAST_ERROR__ = { label, message: String(e), stack: e?.stack };
+        if (DEBUG) {
+          window.__HG_LAST_ERROR__ = {
+            label,
+            message: String(e),
+            stack: e?.stack
+          };
+        }
       });
     }
   } catch (e) {
     console.error(`[${label}]`, e);
-    if (DEBUG) window.__HG_LAST_ERROR__ = { label, message: String(e), stack: e?.stack };
+    if (DEBUG) {
+      window.__HG_LAST_ERROR__ = {
+        label,
+        message: String(e),
+        stack: e?.stack
+      };
+    }
   }
 }
 
