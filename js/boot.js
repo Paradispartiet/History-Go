cf134cd9e61b60baeb2a9ea5914fe8a138a49330
Paradispartiet <paradispartiet@gmail.com>
2026-02-09 14:20:37 +0100

    Update and rename boot(?).js to boot.js

---
+++ js/boot.js
@@ -0,0 +1,24 @@
+export async function bootApp() {
+  await loadBaseData();
+  await loadPeople();
+  await loadEpoker();
+
+  initMap();
+  initPosition();
+  initUI();
+  initQuizzes();
+}
+
+
+function requestLocation() {
+  window.HG_ENV = window.HG_ENV || {};
+  window.HG_ENV.geo = "unknown";
+
+  // vis steder med "–" mens vi venter
+  if (typeof renderNearbyPlaces === "function") renderNearbyPlaces();
+
+  // pro: delegér alt til pos.js
+  if (window.HGPos?.request) return window.HGPos.request();
+
+  console.warn("[geo] HGPos.request mangler (pos.js ikke lastet?)");
+}
