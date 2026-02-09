b299429c27e94176b0b16ab393509e11ec5578fc
Paradispartiet <paradispartiet@gmail.com>
2026-02-09 16:20:23 +0100

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -2,15 +2,3 @@
 
 
 
-function requestLocation() {
-  window.HG_ENV = window.HG_ENV || {};
-  window.HG_ENV.geo = "unknown";
-
-  // vis steder med "–" mens vi venter
-  if (typeof renderNearbyPlaces === "function") renderNearbyPlaces();
-
-  // pro: delegér alt til pos.js
-  if (window.HGPos?.request) return window.HGPos.request();
-
-  console.warn("[geo] HGPos.request mangler (pos.js ikke lastet?)");
-}
