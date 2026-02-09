060f5de0ea6b71961bd4666c976c99d4e4d12bed
Paradispartiet <paradispartiet@gmail.com>
2026-02-09 16:20:53 +0100

    Update geo.js

--- js/core/geo.js
+++ js/core/geo.js
@@ -26,3 +26,17 @@ function distMeters(a, b) {
 
 // global eksponering (samme mønster som resten)
 window.distMeters = distMeters;
+
+
+function requestLocation() {
+  window.HG_ENV = window.HG_ENV || {};
+  window.HG_ENV.geo = "unknown";
+
+  // vis steder med "–" mens vi venter
+  if (typeof renderNearbyPlaces === "function") renderNearbyPlaces();
+
+  // pro: delegér alt til pos.js
+  if (window.HGPos?.request) return window.HGPos.request();
+
+  console.warn("[geo] HGPos.request mangler (pos.js ikke lastet?)");
+}
