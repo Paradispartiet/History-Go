68290e7d4622e4112cb16add4c86eaa1405bc634
Paradispartiet <paradispartiet@gmail.com>
2026-02-10 11:14:46 +0100

    Update CACHE_VERSION to HG-FULL-v3.0.550

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Version: HG-FULL-v3.0.400
 ============================================================ */
 
-const CACHE_VERSION = "HG-FULL-v3.0.549";
+const CACHE_VERSION = "HG-FULL-v3.0.550";
 const STATIC_CACHE = `historygo-${CACHE_VERSION}`;
 
 // Scope path (viktig på GitHub Pages: /History-Go/)
