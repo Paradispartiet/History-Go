017ceef0596bfbb250eecae791d9733200d4212d
Paradispartiet <paradispartiet@gmail.com>
2026-02-20 12:59:35 +0100

    Update bottomSheetController.js

--- js/core/bottomSheetController.js
+++ js/core/bottomSheetController.js
@@ -1,55 +1,45 @@
 (function(){
 
   const STATES = {
-    HIDDEN: "hidden",      // brukes bare av LayerManager
-    OPEN: "open",
-    COLLAPSED: "collapsed"
+    HIDDEN: "hidden",
+    OPEN: "open"
   };
 
   let el = null;
-  let state = STATES.COLLAPSED;
+  let state = STATES.HIDDEN;
 
   function setState(next){
     if (!el) return;
     if (next === state) return;
 
-    el.classList.remove("is-open", "is-collapsed", "is-hidden");
+    el.classList.remove("is-open", "is-hidden");
 
-switch(next){
-  case STATES.OPEN:
-    el.classList.add("is-open");
-    break;
+    switch(next){
+      case STATES.OPEN:
+        el.classList.add("is-open");
+        el.setAttribute("aria-hidden", "false");
+        break;
 
-  case STATES.COLLAPSED:
-    el.classList.add("is-collapsed");
-    break;
-
-  case STATES.HIDDEN:
-    el.classList.add("is-hidden");
-    break;
-}
+      case STATES.HIDDEN:
+        el.classList.add("is-hidden");
+        el.setAttribute("aria-hidden", "true");
+        break;
+    }
 
     state = next;
   }
 
   function open(){
     setState(STATES.OPEN);
-    if (el) el.setAttribute("aria-hidden", "false");
   }
 
-  function collapse(){
-  setState(STATES.COLLAPSED);
-  if (el) el.setAttribute("aria-hidden", "true");
-}
-
-function hide(){
-  setState(STATES.HIDDEN);
-  if (el) el.setAttribute("aria-hidden", "true");
-}
+  function hide(){
+    setState(STATES.HIDDEN);
+  }
 
   function toggle(){
     if (state === STATES.OPEN){
-      collapse();
+      hide();
     } else {
       open();
     }
@@ -59,18 +49,16 @@ function hide(){
     el = document.getElementById("placeCard");
     if (!el) return;
 
-    // start collapsed
-    el.classList.add("is-collapsed");
+    el.classList.add("is-hidden");
     el.setAttribute("aria-hidden", "true");
   }
 
   window.bottomSheetController = {
-  init,
-  open,
-  collapse,
-  hide,
-  toggle,
-  setState
-};
+    init,
+    open,
+    hide,
+    toggle,
+    setState
+  };
 
 })();
