42cc1a9ce635e71837f093d926e9327c585176bb
Paradispartiet <paradispartiet@gmail.com>
2026-02-23 21:41:23 +0100

    Delete js/boot/init.js

--- js/boot/init.js
+++
@@ -1,9 +0,0 @@
-/* /js/boot/init.js */
-
-import { CoreEngine } from "/js/core/CoreEngine.js";
-import { HGEngine } from "/js/historygo/HGEngine.js";
-
-export function initAppEngines() {
-  CoreEngine.init();
-  HGEngine.init();
-}
