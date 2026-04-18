ca234082e5c31d86f9f516866de973237936fa33
Paradispartiet <paradispartiet@gmail.com>
2026-04-18 05:42:01 +0200

    Delete AHA_Twitter directory

--- AHA_Twitter/lib/constants/postTypes.ts
+++
@@ -1,9 +0,0 @@
-export const POST_TYPES = {
-  THOUGHT: 'thought',
-  INSIGHT: 'insight',
-  QUESTION: 'question'
-} as const;
-
-export type PostType = (typeof POST_TYPES)[keyof typeof POST_TYPES];
-
-export const POST_TYPE_OPTIONS = Object.values(POST_TYPES);
