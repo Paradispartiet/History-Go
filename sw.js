fe67b0f1e7cc27866dca5b43a4364096d12c3abe
Paradispartiet <paradispartiet@gmail.com>
2026-04-27 21:19:24 +0200

    Bump service worker cache for header and nearby thumb CSS

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Oppdatert: 2026-04-27
    ============================================================ */
 
-const SW_VERSION = "hg-sw-2026-04-27-v1.38.111";
+const SW_VERSION = "hg-sw-2026-04-27-v1.38.112";
 
 const CACHE_STATIC  = `hg-static-${SW_VERSION}`;
 const CACHE_RUNTIME = `hg-runtime-${SW_VERSION}`;
