69d58840dffdff5ca5205174640656b511e46ed4
Paradispartiet <paradispartiet@gmail.com>
2026-04-28 08:08:13 +0200

    Bump service worker cache for round icon sizing

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Oppdatert: 2026-04-27
    ============================================================ */
 
-const SW_VERSION = "hg-sw-2026-04-27-v1.38.117";
+const SW_VERSION = "hg-sw-2026-04-27-v1.38.118";
 
 const CACHE_STATIC  = `hg-static-${SW_VERSION}`;
 const CACHE_RUNTIME = `hg-runtime-${SW_VERSION}`;
