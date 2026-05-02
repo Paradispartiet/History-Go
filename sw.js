4a33d4d5d72a0589c0dd7706eda717d94f0ca073
Paradispartiet <paradispartiet@gmail.com>
2026-05-02 23:02:55 +0200

    Update service worker version to v1.39.200

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Oppdatert: 2026-05-01
    ============================================================ */
 
-const SW_VERSION = "hg-sw-2026-05-01-v1.39.199";
+const SW_VERSION = "hg-sw-2026-05-01-v1.39.200";
 
 const CACHE_STATIC  = `hg-static-${SW_VERSION}`;
 const CACHE_RUNTIME = `hg-runtime-${SW_VERSION}`;
