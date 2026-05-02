7994705d26fb4b25c1fc3996e29afa397efb8e92
Paradispartiet <paradispartiet@gmail.com>
2026-05-02 16:38:40 +0200

    Update MapTiler browser key in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Lim inn MapTiler browser-key her, ikke hele style-URL-en.
-window.HG_MAPTILER_KEY = "Yi8j8sLhEo4Ny";
+window.HG_MAPTILER_KEY = "Yi8j8sLhEo4NyPygVmbN";
 
 // Detaljert kartstil: bruk MapTiler style-id her, f.eks. "streets-v4", "outdoor-v2" eller "basic-v2".
 // Ikke legg API-key her.
