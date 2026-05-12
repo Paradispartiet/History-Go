8767c0f7c6cc30c1034950862cb7fb68d62ec246
Paradispartiet <paradispartiet@gmail.com>
2026-05-12 03:13:07 +0200

    Add read-only HGKnowledgeEngine analysis module

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Oppdatert: 2026-05-02
    ============================================================ */
 
-const SW_VERSION = "hg-sw-2026-05-11-v1.40.108";
+const SW_VERSION = "hg-sw-2026-05-12-v1.40.109";
 
 const CACHE_STATIC  = `hg-static-${SW_VERSION}`;
 const CACHE_RUNTIME = `hg-runtime-${SW_VERSION}`;
@@ -62,6 +62,8 @@ const PRECACHE_URLS = [
 
   "js/DomainRegistry.js",
   "js/fagHealthReport.js",
+  "js/courses.js",
+  "js/hgKnowledgeEngine.js",
   "js/domainHealthReport.js",
 
   // Dev / console
