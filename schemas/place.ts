c79fad1d13139ba069e636e41035d6f2d1b89b55
Paradispartiet <paradispartiet@gmail.com>
2026-05-13 12:27:52 +0200

    Add sport profile field to place schema

--- schemas/place.ts
+++ schemas/place.ts
@@ -1,3 +1,13 @@
+export interface PlaceSportProfile {
+  groundhopper_relevant?: boolean;
+  sports?: string[];
+  venue_kind?: string;
+  groundhopper_type?: string;
+  clubs_or_teams?: string[];
+  teams?: string[];
+  [key: string]: unknown;
+}
+
 export interface Place {
   id: string;
   name?: string;
@@ -20,6 +30,7 @@ export interface Place {
   emne_ids?: string[];
   hidden?: boolean;
   stub?: boolean;
+  sport_profile?: PlaceSportProfile;
   quiz_profile?: Record<string, unknown>;
   relations?: unknown[];
   people?: unknown[];
