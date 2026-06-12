ec1bd20f1989b50dd8a2178ab428aa8eb1cf3d2f
Paradispartiet <paradispartiet@gmail.com>
2026-06-12 11:41:54 +0200

    Load city stories batch manifest

--- js/stories/stories_loader.js
+++ js/stories/stories_loader.js
@@ -2,7 +2,8 @@
   const MANIFEST_PATH = "data/stories/stories_manifest.json";
   const EXTRA_MANIFEST_PATHS = [
     "data/stories/stories_manifest_music_batch_01.json",
-    "data/stories/stories_manifest_naeringsliv_batch_01.json"
+    "data/stories/stories_manifest_naeringsliv_batch_01.json",
+    "data/stories/stories_manifest_by_batch_01.json"
   ];
 
   function ensureArray(value) {
