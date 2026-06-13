92975b47687436d0a28f88ed67decd8e7b1b32c7
Paradispartiet <paradispartiet@gmail.com>
2026-06-13 14:10:27 +0200

    Load sport stories batch manifest

--- js/stories/stories_loader.js
+++ js/stories/stories_loader.js
@@ -3,7 +3,8 @@
   const EXTRA_MANIFEST_PATHS = [
     "data/stories/stories_manifest_music_batch_01.json",
     "data/stories/stories_manifest_naeringsliv_batch_01.json",
-    "data/stories/stories_manifest_by_batch_01.json"
+    "data/stories/stories_manifest_by_batch_01.json",
+    "data/stories/stories_manifest_sport_batch_01.json"
   ];
 
   function ensureArray(value) {
