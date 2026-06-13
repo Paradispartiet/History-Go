ac65a280439ad571f4baf32193bcf18b5527219b
Paradispartiet <paradispartiet@gmail.com>
2026-06-13 14:39:53 +0200

    Load subculture stories batch manifest

--- js/stories/stories_loader.js
+++ js/stories/stories_loader.js
@@ -6,7 +6,8 @@
     "data/stories/stories_manifest_by_batch_01.json",
     "data/stories/stories_manifest_sport_batch_01.json",
     "data/stories/stories_manifest_litteratur_media_batch_01.json",
-    "data/stories/stories_manifest_vitenskap_batch_01.json"
+    "data/stories/stories_manifest_vitenskap_batch_01.json",
+    "data/stories/stories_manifest_subkultur_batch_01.json"
   ];
 
   function ensureArray(value) {
