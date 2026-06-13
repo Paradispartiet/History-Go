034235b73c74cdadce6b299f2933428ab260b767
Paradispartiet <paradispartiet@gmail.com>
2026-06-13 23:43:10 +0200

    Load Lisbon business batch manifest

--- js/stories/stories_loader.js
+++ js/stories/stories_loader.js
@@ -15,7 +15,8 @@
     "data/stories/stories_manifest_historie_batch_01.json",
     "data/stories/stories_manifest_lisboa_historie_batch_01.json",
     "data/stories/stories_manifest_litteratur_batch_02.json",
-    "data/stories/stories_manifest_musikk_lisboa_batch_01.json"
+    "data/stories/stories_manifest_musikk_lisboa_batch_01.json",
+    "data/stories/stories_manifest_naeringsliv_lisboa_batch_01.json"
   ];
 
   function ensureArray(value) {
