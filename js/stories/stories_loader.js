1a26b4289cb96b8dc93d7fa1bca97e74d8890d31
Paradispartiet <paradispartiet@gmail.com>
2026-06-16 23:38:51 +0200

    Load Lisbon film TV batch 01 manifest

--- js/stories/stories_loader.js
+++ js/stories/stories_loader.js
@@ -47,7 +47,8 @@
     "data/stories/stories_manifest_vitenskap_lisboa_batch_03.json",
     "data/stories/stories_manifest_natur_lisboa_batch_03.json",
     "data/stories/stories_manifest_by_lisboa_batch_06.json",
-    "data/stories/stories_manifest_media_lisboa_batch_01.json"
+    "data/stories/stories_manifest_media_lisboa_batch_01.json",
+    "data/stories/stories_manifest_film_tv_lisboa_batch_01.json"
   ];
 
   function ensureArray(value) {
