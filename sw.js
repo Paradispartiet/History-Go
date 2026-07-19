cf2c769dd1df103dc983fbc348cb71c3100ee933
Paradispartiet <paradispartiet@gmail.com>
2026-07-19 11:01:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Oppdatert: 2026-06-22
    ============================================================ */
 
-const SW_VERSION = "hg-sw-2026-07-07-v1.3.119";
+const SW_VERSION = "hg-sw-2026-07-07-v1.3.120";
 
 const CACHE_STATIC  = `hg-static-${SW_VERSION}`;
 const CACHE_RUNTIME = `hg-runtime-${SW_VERSION}`;
