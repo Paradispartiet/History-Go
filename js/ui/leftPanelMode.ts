08bc1ce0be876ffde94e174ff710466374d11059
Paradispartiet <paradispartiet@gmail.com>
2026-09-11 16:42:07 +0200

    Keep Explore panel compatible with web TS target

--- js/ui/leftPanelMode.ts
+++ js/ui/leftPanelMode.ts
@@ -87,11 +87,11 @@ function cleanText(value: unknown): string {
 
 function escapeHtml(value: unknown): string {
   return cleanText(value)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 function list<T = unknown>(value: unknown): T[] {
