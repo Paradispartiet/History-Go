449c3dfc0a4fc22cc78f6d21335c30783a5784d3
Paradispartiet <paradispartiet@gmail.com>
2026-09-11 13:03:32 +0200

    Complete Historievandrer count update in civication-noncareer-role-taxonomy.test.js

--- tests/civication-noncareer-role-taxonomy.test.js
+++ tests/civication-noncareer-role-taxonomy.test.js
@@ -216,7 +216,7 @@ const expectedCounts = {
   relationship_stages: 6,
   career_role_worlds: 85,
   life_position_role_worlds: 5,
-  total_role_worlds: 89,
+  total_role_worlds: 90,
   role_model_manifest_files: 293,
   canonical_scenario_role_models: 287,
   shadowed_role_models: 6,
